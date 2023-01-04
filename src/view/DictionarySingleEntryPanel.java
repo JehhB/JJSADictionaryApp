2fa6e6e4ac6f2c779d5119a99d87185d32399e62
JehhB <jwy.bayod@gmail.com>
2023-01-04 17:09:15 +0800

    Resized components

--- src/view/DictionarySingleEntryPanel.java
+++ src/view/DictionarySingleEntryPanel.java
@@ -35,11 +35,11 @@ public class DictionarySingleEntryPanel extends javax.swing.JPanel implements Li
                 this.setLayout(layout);
                 layout.setHorizontalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 400, Short.MAX_VALUE)
+                        .addGap(0, 422, Short.MAX_VALUE)
                 );
                 layout.setVerticalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 300, Short.MAX_VALUE)
+                        .addGap(0, 100, Short.MAX_VALUE)
                 );
         }// </editor-fold>//GEN-END:initComponents
 
--- src/view/FooterPanel.java
+++ src/view/FooterPanel.java
@@ -26,15 +26,17 @@ public class FooterPanel extends javax.swing.JPanel {
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
+                setBackground(new java.awt.Color(39, 68, 114));
+
                 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
                 this.setLayout(layout);
                 layout.setHorizontalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 400, Short.MAX_VALUE)
+                        .addGap(0, 768, Short.MAX_VALUE)
                 );
                 layout.setVerticalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 300, Short.MAX_VALUE)
+                        .addGap(0, 56, Short.MAX_VALUE)
                 );
         }// </editor-fold>//GEN-END:initComponents
 
